5b01263fed1f7945ca9e0c3bdec20a888b19ef41
root <root@bd99c3edc11c.mylabserver.com>
2024-11-29 09:13:26 +0000

    Fix

--- vpc-tgw/__main__.py
+++ vpc-tgw/__main__.py
@@ -260,6 +260,7 @@ for pool_id in range(4):
 
 
 tgw_peerings = []
+tgw_peerings_accepter = []
 
 for i in range(1, 4):
     peering = TGWATTACHMENT(
@@ -272,17 +273,18 @@ for i in range(1, 4):
     )
 
     tgw_peerings.append(peering)
+    tgw_peerings_accepter.append(peering.accepter)
 
 i = 0
 
 for attachment in tgw_peerings:
     vpc_cidr = f"172.31.{(i+1)*16}.0/20"
     i += 1
-    aws_tf.ec2transitgateway.Route(f"staticRoute-${i}",
+    aws_tf.ec2transitgateway.Route(f"staticRoute-{i}",
                                            destination_cidr_block=vpc_cidr,
                                            transit_gateway_attachment_id=attachment.id,
                                            transit_gateway_route_table_id=tgw_rts[0],
-                                           #opts=pulumi.ResourceOptions(depends_on=tgw_peerings),
+                                           opts=pulumi.ResourceOptions(depends_on=tgw_peerings),
              )
 
 
