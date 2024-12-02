34fd92519f235f0d44cdae48a21614e068243ef8
root <root@bd99c3edc11c.mylabserver.com>
2024-12-02 11:51:55 +0000

    Improve test

--- ec2/__main__.py
+++ ec2/__main__.py
@@ -16,12 +16,12 @@ this = aws.ec2.get_ami(most_recent=True,
 
 deploy = aws.ec2.KeyPair("deploy",
     key_name="deploy",
-    public_key="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDbn1bCFoD5oiWokQ25b6S85tPwxoUFZZHu5rcF+PtSZOzZALit2o2TmbQw7SWWw+a2Q+GWV9+ATRFq3pjEIyYd55ONNs2DOCio2xDDfDMVhFxn2+bJJQptlzgMIiXBKptcgay9d5HgRNyIyDSpf1rIUYIQHOGKo91/jPmu8X3DdYSap59sX62KvM+gwYVJFL1b4POKTCxcOYVO81PHU3uVTtQD9HZ88ISEwsZLoPdfld5Zkd1kyeEyESOIKw5q7odXOKYBU6enyOaC6J+uDnrrJ588j3iygXwB6OnZ+ibPg064VHs+AfTTRYw1H4ciBXJTGsROI6RADg6M3UG0d5NynjZHPZ5p/IjGPkpaF3YJNnuG6H/TGkq7Qs6AB5VFaq6tjhfM7e20IW3lPc9JmU/kdUXrZkxiZzt44quUyQpxcka4SX0a8NuuJFHujw+ds8ja3M3Go0qDa8G4zE/ZCwukIzJE68+duGV8/K5TInRoTZkiuetQrU+/7lvBGqxEoQM=")
+    public_key="TOCHANGE")
 
 
-def create_sg(vpc_id, a=""):
+def create_sg(vpc_id, a):
     allow_ssh = aws.ec2.SecurityGroup(f"allow_ssh{a}",
-        name="allow_ssh",
+        name=f"allow_ssh{a}",
         description="Allow SSH inbound traffic and all outbound traffic",
         vpc_id=vpc_id,
         tags={
@@ -40,21 +40,52 @@ def create_sg(vpc_id, a=""):
                                     )
     return allow_ssh
 
-def create_ec2(subnet_id, allow_ssh, a=""):
-    this_instance = aws.ec2.Instance(f"this{a}",
+def create_ec2(subnet_id, allow_ssh, name):
+    this_instance = aws.ec2.Instance(name,
         ami=this.id,
         instance_type=aws.ec2.InstanceType.T4G_NANO,
         subnet_id=subnet_id,
         key_name="deploy",
         vpc_security_group_ids=[allow_ssh.id]
     )
+    return this_instance
 
-vpc_id = "vpc-06fe3f61362e32f92"
-subnet_id = "subnet-03d4a3a299a4e535c"
-allow_ssh = create_sg(vpc_id)
-create_ec2(subnet_id, allow_ssh)
-create_ec2("subnet-02a40587e21e65b6b", allow_ssh, a="ddd")
+def get_vpc_id(cidr):
+    vpc = aws.ec2.get_vpc(cidr_block=cidr)
+    return vpc.id
 
-allow_ssh = create_sg("vpc-0fd5d5de5214942a0", a="ddd")
-create_ec2("subnet-0f4bdab3a5c556488", allow_ssh, a="ccc")
-create_ec2("subnet-0770aa1a7d196003a", allow_ssh, a="eee")
+def get_subnet_id(vpc_id, subnet_type, subnet_id=0):
+    subnet = aws.ec2.get_subnet(vpc_id=vpc_id, tags={"Name": f"subnet-{subnet_type}-1-*"})
+    return subnet.id
+
+def create_bastion(cidr):
+    vpc_id = get_vpc_id(cidr)
+    public_subnet_id = get_subnet_id(vpc_id, "public")
+    allow_ssh = create_sg(vpc_id, "bastion")
+    public_instance = create_ec2(public_subnet_id, allow_ssh, "public_instance")
+    return public_instance.public_ip
+
+def create_instance(cidr, vpc_name):
+    vpc_id = get_vpc_id(cidr)
+    subnet_id = get_subnet_id(vpc_id, "private")
+    allow_ssh = create_sg(vpc_id, vpc_name)
+    private_instance = create_ec2(subnet_id, allow_ssh, f"private_instance{vpc_name}")
+    return private_instance.private_ip
+
+cidrs = [
+        "172.31.0.0/20",
+        "172.31.16.0/20",
+        "172.31.32.0/20",
+        "172.31.48.0/20",
+        "172.31.64.0/20",
+        ]
+
+public_ip = create_bastion(cidrs[0])
+ip1 = create_instance(cidrs[0], "1")
+ip2 = create_instance(cidrs[1], "2")
+ip3 = create_instance(cidrs[2], "3")
+
+pulumi.export("public_ip", public_ip)
+pulumi.export("ip1", ip1)
+pulumi.export("ip2", ip2)
+pulumi.export("ip3", ip3)
